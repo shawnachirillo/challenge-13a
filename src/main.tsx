66710165384661847bed127fb97ba85bfed3125c
Shawna Chirillo <schirillo711@gmail.com>
2025-03-30 12:41:36 -0500

    Final submission - challenge complete

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CandidateSearch from './pages/CandidateSearch';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getGitHubUsers } from '../api/API';
 import CandidateCard from '../components/CandidateCard';
 
